4e292de70fcc6b8eae238253a4027745314d921e
Santiago Pravisani <santiagopravisani@gmail.com>
2021-01-17 22:59:57 +0900

    App skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Header from './Header/Header'
+import Banner from './Banner/Banner'
+import Services from './Services/Services'
+import Team from './Team/Team'
+import Portfolio from './Portfolio/Portfolio'
+import Contact from './Contact/Contact'
 
 function App() {
   return (
     <div className="App">
-      <h1>CRA SanaCompany</h1>
+      <Header />
+      <Banner />
+      <Services />
+      <Team />
+      <Portfolio />
+      <Contact />
     </div>
   )
 }
