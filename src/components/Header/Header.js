03a50f9d6fac39d75a721a2ea78de6866b6b29c6
Santiago Pravisani <santiagopravisani@gmail.com>
2021-01-18 00:06:16 +0900

    Mobile header sketch

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,36 @@
 import React from 'react'
+import { ReactComponent as Logo } from 'assets/images/logo.svg'
 
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <div className="header">
+      <div className="header__content">
+        <button type="button" className="btn header__logo-link">
+          <Logo className="header__logo" />
+        </button>
+        <ul className="header__list">
+          <li className="header__item">
+            <a href="#products" className="header__link">
+              Services
+            </a>
+          </li>
+          <li className="header__item">
+            <a href="#sign-in" className="header__link">
+              Portfolio
+            </a>
+          </li>
+          <li className="header__item">
+            <a href="#contact" className="header__link">
+              Team
+            </a>
+          </li>
+          <li className="header__item">
+            <a href="#contact" className="header__link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
