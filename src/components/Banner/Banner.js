2b6812ef802016420957fbb6551eb83eae322a70
Santiago Pravisani <santiagopravisani@gmail.com>
2021-02-01 20:26:13 +0900

    Pixel perfect Banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,6 +5,10 @@ import animationData from './animation.json'
 export default function Banner() {
   return (
     <div className="banner">
+      <div className="banner__title">
+        We make your ideas
+        <span className="banner__title text-secondary"> true</span>
+      </div>
       <LottiePlayer animationData={animationData} className="banner__animation" />
     </div>
   )
