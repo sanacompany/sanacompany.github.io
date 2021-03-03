0e7d007e1d4e875cea7ca1b5f9b06d02cd748e18
Francisco Masi <masifrancisco@gmail.com>
2021-03-03 14:05:00 -0300

    Creada funcion para definir estructura de layout, creado componente para reiniciar scroll ante cambio de pagina

--- src/components/App.js
+++ src/components/App.js
@@ -4,30 +4,64 @@ import {
   Switch,
   Route,
 } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import * as ROUTES from 'constants/Urls'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 import Home from 'components/Home'
 import ProjectDetail from 'components/Project/ProjectDetailContainer'
+import ScrollToTop from 'components/Shared/ScrollToTop'
 
+const Section = ({ children, internal }) => {
+  if (internal) {
+    return (
+      <>
+        <div className="main">
+          {children}
+        </div>
+        <Footer internal />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <div className="main">
+        {children}
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+Section.propTypes = {
+  children: PropTypes.number.isRequired,
+  internal: PropTypes.bool,
+}
+
+Section.defaultProps = { internal: false }
+
+// Main
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="main">
-        <Router basename="/">
-          <Switch>
-            <Route path={ROUTES.PROJECT}>
+      <Router basename="/">
+        <ScrollToTop />
+        <Switch>
+          <Route path={ROUTES.PROJECT}>
+            <Section internal>
               <ProjectDetail />
-            </Route>
-            <Route path={ROUTES.HOME}>
+            </Section>
+          </Route>
+          <Route path={ROUTES.HOME}>
+            <Section>
               <Home />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-      <Footer />
+            </Section>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
